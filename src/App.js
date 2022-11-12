e0ca568a27660d4a02744f2c1eda45e2fc0d11c7
Pabasara Jayawardhana <pabasarajayawardhana@gmail.com>
2022-11-12 10:25:23 -0500

    Add search, filter and pagination options to a university list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import uniqid from 'uniqid';
+
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { InputAdornment } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+
+import debounce from 'lodash.debounce';
+import axios from 'axios';
+
+import UniversityList from './components/UniversityList'
+
+const baseURL = "http://universities.hipolabs.com/search";
+
+const HeaderWrapperDiv = styled('div')(({ theme }) => ({
+  padding: '20px'
+}));
+
+const TitleWrapperDiv = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center'
+}));
+
+const SearchWrapperDiv = styled('div')(({ theme }) => ({
+  float: 'left',
+  paddingTop: '10px'
+}));
+
+const SelecterWrapperDiv = styled('div')(({ theme }) => ({
+  float: 'right'
+}));
 
 function App() {
+
+  const [universities, setUniversities] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [code, setCode] = useState('Select by Code');
+  const [codeList, setCodeList] = useState([]);
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+
+    // Get data using fetch
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setLoaded(true);
+          setUniversities(result);
+
+          const filteredCodes = [...new Set(result.map(item => item.alpha_two_code))]
+          setCodeList(filteredCodes)
+        }
+      )
+
+
+    // Get data using axios
+    /*
+    axios.get(baseURL)
+      .then(
+        (result) => {
+          setLoaded(true);
+          setUniversities(result.data);
+
+          const filteredCodes = [...new Set(result.map(item => item.alpha_two_code))]
+          setCodeList(filteredCodes)
+        }
+      ) 
+      */  
+
+  }
+
+  const handleSearch = debounce((serchTerm) => {
+
+    let formatSerchTerm = serchTerm.split(' ').join('+')
+
+    fetch(`${baseURL}?country=${formatSerchTerm}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setLoaded(true);
+          setUniversities(result)
+        }
+      )
+
+      // Get data using axios with parameters
+      /*
+      axios.get(baseURL, {
+        params: {
+          country: formatSerchTerm
+        }
+      })
+      .then(
+        (result) => {
+          setLoaded(true);
+          setUniversities(result.data);
+
+          const filteredCodes = [...new Set(result?.data.map(item => item.alpha_two_code))]
+          setCodeList(filteredCodes)
+        }
+      )
+      */
+
+  }, 1000)
+
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const byCategory = (university, code) => {
+    if (code !== 'Select by Code') {
+      return university.alpha_two_code === code;
+    } else return university;
+  };
+
+  const handleUniList = (universities, code) => {
+    return universities
+      .filter(university => byCategory(university, code))
+  }
+
+  if (!loaded) {
+    return <>loading...</>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed>
+      <Box sx={{ flexGrow: 1 }} >
+        <HeaderWrapperDiv >
+          <TitleWrapperDiv>
+            <Typography variant="h4" gutterBottom>
+              Universities
+            </Typography>
+          </TitleWrapperDiv>
+          <div>
+            <SearchWrapperDiv>
+              <TextField
+                size="small"
+                type="search"
+                name="search"
+                id="outlined-basic"
+                label="Search by Country"
+                variant="outlined"
+                onChange={e => handleSearch(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </SearchWrapperDiv>
+            <SelecterWrapperDiv>
+              <FormControl sx={{ m: 1, minWidth: 180 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Code</InputLabel>
+                <Select
+                  size="small"
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                  value={code}
+                  onChange={handleCodeChange}
+                  autoWidth
+                  label="Code"
+                  defaultValue={code}
+                >
+                  <MenuItem value="Select by Code">Select by Code</MenuItem>
+                  {codeList?.map(code => (
+                    <MenuItem key={uniqid()} value={code}>{code}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </SelecterWrapperDiv>
+          </div>
+          <UniversityList uniList={handleUniList(universities, code)} />
+        </HeaderWrapperDiv>
+      </Box>
+    </Container>
   );
 }
+
 
 export default App;
---
+++ src/components/UniversityList.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import uniqid from 'uniqid';
+
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+
+import { experimentalStyled as styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '80%'
+}));
+
+const UrlList = styled(List)({
+    display: 'flex',
+    justifyContent: 'center'
+});
+
+const UrlListItem = styled(ListItemButton)({
+    padding: '0',
+    color: 'blue'
+});
+
+const LoadMoreWrapper = styled('div')(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '20px'
+}));
+
+export default function UniversityList(props) {
+
+    const [paginate, setpaginate] = useState(9);
+
+    const { uniList } = props;
+
+    const load_more = () => {
+        setpaginate((prevValue) => prevValue + 9);
+    };
+
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
+    return (
+        <>
+            <Grid container spacing={0.5} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {uniList.slice(0, paginate).map(uni => (
+                    <Grid item xs={2} sm={4} md={4} key={uniqid()}>
+                        <Item>
+                            <Typography variant="h6" color="text.secondary">
+                                {uni.name}
+                            </Typography>
+                            <Typography variant="overline" display="block" gutterBottom>
+                                {uni.country}
+                            </Typography>
+
+                            <UrlList>
+                                <div >
+                                    {uni.web_pages?.map(page => (
+                                        <UrlListItem size="small" key={uniqid()} onClick={() => openInNewTab(page)}>
+                                            {page}
+                                        </UrlListItem>
+                                    ))}
+                                </div>
+                            </UrlList>
+
+                        </Item>
+                    </Grid>
+                ))}
+
+            </Grid>
+            <LoadMoreWrapper>
+                <Button variant="contained" color="success" onClick={load_more}>Load More</Button>
+            </LoadMoreWrapper>
+
+        </>
+    )
+}
